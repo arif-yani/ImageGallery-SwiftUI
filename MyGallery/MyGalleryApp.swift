cd36603909a20660e04994af7ca355ffde82a018
arif-yani <84135413+arif-yani@users.noreply.github.com>
2022-09-30 13:26:42 +0700

    Initial Commit

---
+++ MyGallery/MyGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGalleryApp.swift
+//  MyGallery
+//
+//  Created by Muhamad Arif on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
